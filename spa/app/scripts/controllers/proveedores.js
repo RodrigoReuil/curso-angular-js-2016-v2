8ed4fc48bcea11322f25d4af13c40b7ae37a0a51
Cristian Cortez <cortez.cristian@gmail.com>
2016-06-30 19:56:20 -0300

    Servicios alta listado

--- spa/app/scripts/controllers/proveedores.js
+++ spa/app/scripts/controllers/proveedores.js
@@ -8,7 +8,23 @@
  * Controller of the spaApp
  */
 angular.module('spaApp')
-  .controller('ProveedoresCtrl', function () {
+  .controller('ProveedoresCtrl', function ($scope, provServ) {
+
+    $scope.proveedores = [];
+    $scope.prov = {};
+
+    provServ.all().then(function(res){
+      $scope.proveedores = res;
+    });
+
+    $scope.altaProveedor = function() {
+      // Crear 1 proveedor
+      provServ.create($scope.prov).then(function(res){
+        alert("Guardado exitosamente");
+        $scope.prov.nombre = '';
+      });
+    }
+
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
---
+++ spa/app/scripts/services/proveedores.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name spaApp.proveedores
+ * @description
+ * # proveedores
+ * Factory in the spaApp.
+ */
+angular.module('spaApp')
+  .factory('provServ', function ($http, $q, $log) {
+    // Service logic
+    // ...
+
+    var meaningOfLife = 42;
+
+    // Public API here
+    return {
+      all: function(){
+        var prom = $q.defer();
+
+        $http({
+           method: 'GET',
+           url: 'http://localhost:9001/proveedores'
+        }).then(function(res){
+          $log.log("GET /proveedores ", res.data);
+          prom.resolve(res.data);
+        }, function(err){
+          prom.reject(err);
+        });
+
+        return prom.promise;
+      },
+      create: function(datos){
+        var prom = $q.defer();
+
+        $http({
+           method: 'POST',
+           data: datos,
+           url: 'http://localhost:9001/proveedores'
+        }).then(function(res){
+          $log.log("POST /proveedores ", res.data);
+          prom.resolve(res.data);
+        }, function(err){
+          prom.reject(err);
+        });
+
+        return prom.promise;
+
+      },
+      someMethod: function () {
+        return meaningOfLife;
+      }
+    };
+  });
